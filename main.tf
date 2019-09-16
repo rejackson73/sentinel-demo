b2807c12bea5889095beae3d8b8f0d8179ad0680
Rob Jackson <28791872+rejackson73@users.noreply.github.com>
2019-09-16 13:30:07 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,6 +34,7 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   tags = {
-    Name = "Wolverine"
+    Name = "rjackson"
+    TTL = "72"
   }
 }
