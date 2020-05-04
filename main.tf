338ecd694e7d81f213710f31bbca1387add9446b
Rob Jackson <28791872+rejackson73@users.noreply.github.com>
2020-05-04 11:06:27 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,10 +30,64 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+resource aws_vpc "rj-demo" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+
+  tags = {
+    name = "${var.prefix}-vpc"
+  }
+}
+
+resource aws_subnet "rj-demo" {
+  vpc_id     = aws_vpc.nomad-demo.id
+  cidr_block = var.vpc_cidr
+  tags = {
+    name = "${var.prefix}-subnet"
+  }
+}
+
+resource aws_security_group "rj-demo" {
+  name = "${var.prefix}-security-group"
+
+  vpc_id = aws_vpc.rj-demo.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name = "${var.prefix}-security-group"
+  }
+}
+
 resource "aws_instance" "compute" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.large"
+  instance_type               = var.instance_type
+  key_name                    = var.aws_key
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.rj-demo.id
+  vpc_security_group_ids      = [aws_security_group.rj-demo.id]
   tags = {
     Owner = "rjackson"
+    TTL = 1
   }
 }
