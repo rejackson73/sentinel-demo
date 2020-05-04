2a8bdcfba4c1476a404fc225027693b5b5ca188c
Rob Jackson <28791872+rejackson73@users.noreply.github.com>
2020-05-04 14:56:00 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 output "private_ip_addr" {
-  value       = aws_instance.server.private_ip
+  value       = aws_instance.compute.private_ip
   description = "The private IP address of the main server instance."
 }
 output "public_ip_addr" {
-  value       = aws_instance.server.public_ip
-  description = "The private IP address of the main server instance."
+  value       = aws_instance.compute.public_ip
+  description = "The public IP address of the main server instance."
 }
